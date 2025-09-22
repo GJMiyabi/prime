eab2c1d3350b564d25072b276d4df645e87c5bd2
GJ <40647866+GJMiyabi@users.noreply.github.com>
2025-09-22 17:47:42 +0900

    組織ページ追加

--- web/src/app/organization/[organizationId]/page.tsx
+++ web/src/app/organization/[organizationId]/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { Header } from "@/app/_components/common";
-import PersonCreateForm from "@/app/person/_components/person/form/create";
+import {
+  CollapsibleCardContainer,
+  CardContent,
+} from "@/app/_components/common";
 
 export default function Page() {
   return (
     <div>
       <Header />
       <div className="pt-[30px]">
-        <PersonCreateForm />
+        <CollapsibleCardContainer title="組織">
+          <div className="py-[5px]">
+            <CardContent title="名称" value="" />
+          </div>
+        </CollapsibleCardContainer>
       </div>
     </div>
   );
